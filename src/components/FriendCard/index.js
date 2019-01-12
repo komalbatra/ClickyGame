31757d2648ccf68ec1bbe32a4777543037cb1181
Komal Batra <KomalBatra@gmail.com>
2019-01-12 10:02:30 -0500

    Layout updated

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default FriendCard;
